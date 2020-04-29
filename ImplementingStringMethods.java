36745788e91a92e7704796d04272ea4373781a12
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-04-29 01:08:56 -0500

    Implementing String Methods

---
+++ ImplementingStringMethods.java
@@ -0,0 +1,18 @@
+public class ImplementingStringMethods {
+	public static void main(String[] ar){
+	String a = "Sky";
+	String b = "is blue.";
+	System.out.println("A String is = " +a);
+	System.out.println("B String is = " +b);
+	System.out.println("Concatinating a and b = " + a.concat(b));
+	System.out.println("Length of a = " + a.length());
+	System.out.println("index of u = " + b.indexOf('u'));
+	System.out.println("UpperCase of b = " + b.toUpperCase());
+	System.out.println("LowerCase of a = " + a.toLowerCase());
+	System.out.println("B ends with = " + b.endsWith("."));
+}
+}
+
+
+
+
