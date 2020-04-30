1b2fdd9c2c3bfc6065b68f2cc8b9a2ec1eefa425
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-04-30 01:40:19 -0500

    Operators in Java

---
+++ Operators.java
@@ -0,0 +1,32 @@
+public class Operators {
+ public static void main(String args[]) {
+      int a=10;
+      int b=4;
+      System.out.println(a++);
+      System.out.println(b--);
+      System.out.println(++a);
+      System.out.println(++b);
+      System.out.println(a + b -(a*b) / b);
+      System.out.println(a != b);
+      System.out.println(a+b);
+      System.out.println(a*b);
+      System.out.println(a%b);
+      System.out.println(a>=b);
+      System.out.println(b/a);
+      System.out.println(b++ + ++a);
+      System.out.println(--a - --b);
+      System.out.println(~b);
+      System.out.println(~a);
+      System.out.println(a<<b);
+      System.out.println(10<<3);
+      System.out.println(a>>b);
+      System.out.println(10>>3);
+      System.out.println(-a>>b);
+      System.out.println(a>b&b>a);
+      System.out.println(a<b&b>a);
+      System.out.println(a<b&&a<b);
+      System.out.println(a<b&&a++<b);
+      System.out.println(a<b&a++<b);
+}
+}
+      
