75e13bc0b30d9d3faa19a8069e7959c9fa9cf139
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-04-30 01:39:47 -0500

    if else condition

---
+++ IfElseCondition.java
@@ -0,0 +1,15 @@
+public class IfElseCondition {
+ public static void main(String[] args){
+   int Hari;
+   Hari = 21;
+   
+   if(Hari>=21) {
+      System.out.println("Hari can buy smoke and beer.");
+      System.out.println("He is legal to smoke and drink beer.");
+}   
+   else{
+      System.out.println("Hari cannot buy smoke and beer.");
+      System.out.println("He have to wait till he be 21.");
+      }
+  }
+}
