3853fe0ba5836faf83a5fe5bfff219c44c19dc8b
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-05-01 02:30:21 -0500

    Multiplication Table

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
 public class WhileLoop {
 
     public static void main(String[] args) {
-
-        int n, i;
-        n = 2; 
-        i = 1;
-        while(i <= 10)
-        {
-            System.out.printf("%d * %d = %d \n", n, i, n * i);
+     Scanner S = new Scanner(System.in);
+      System.out.print("Enter the number to find the multiplication table: ");
+      int K = S.nextInt();
+      System.out.println("Multiplication of "+ K + " is :");
+        
+ 
+        int N;
+        int i = 1;
+        while(i <=10){
+            N = K * i; 
+        
+        System.out.println(K + " * " + i + " = " + N);
             i++;
         }
     }
