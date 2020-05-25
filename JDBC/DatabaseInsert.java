11812243b49d37400aabf790c37bc0b1ac7acc9a
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-05-24 22:37:04 -0500

    inner join

--- JDBC/DatabaseInsert.java
+++ JDBC/DatabaseInsert.java
@@ -1,26 +1,27 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+
+package JDBC;
+import java.sql.*;
+
+//Importing the connection package.
+import Connection.*;
+
+
 public class DatabaseInsert {
-
-		// TODO Auto-generated method stub
-		public static void main(String[] args) throws Exception {
-			// TODO Auto-generated method stub
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url="jdbc:mysql://localhost:3306/jnit?serverTimezone=UTC";
-			String username="root";
-			String password="mysql2020";
-			Connection con=DriverManager.getConnection(url,username,password);
-			if(con!=null)
-				System.out.println("Connection Established.");
-			Statement s = con.createStatement();
-			int a = s.executeUpdate("insert into employee values(2334,'Shiva', 70000)");
-			int b = s.executeUpdate("insert into employee values(2335,'Keshab', 71000)");
-			if(a!=0) {
-				System.out.println("Record Inserted.");
-				
-			}
-		}	
+	public static void main (String args[]) throws Exception
+	
+	{
+		MySQL exec = new MySQL();
+		Connection conn = exec.MySQL();
+		Statement s = conn.createStatement();
+		
+		ResultSet rs = s.executeQuery("select a.id,a.name,a.salary,b.deptid,b.name,c.name from jnit a inner join jnit_dept b on a.id=b.id inner join jnitm c on b.deptid=c.deptid;");
+		System.out.println("ID\tEmpName\tSalary\tDeptID\tDeptName\tManagerName");
+		while(rs.next())
+		{
+			System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3)+"\t"+rs.getInt(4)+"\t"+rs.getString(5)+"\t"+rs.getString(6));
+			
 		}
-
-
+	}
+	
+	
+}
