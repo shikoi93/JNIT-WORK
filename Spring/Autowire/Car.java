7a06b74e15c13e98c58f4877ac87fd05e2d30f68
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-06-14 18:47:04 -0500

    Add files via upload

---
+++ Spring/Autowire/Car.java
@@ -0,0 +1,20 @@
+package com.autowire;
+
+public class Car {
+	private String carname;
+	private int carprice;
+	public String getCarname() {
+		return carname;
+	}
+	public void setCarname(String carname) {
+		this.carname = carname;
+	}
+	public int getCarprice() {
+		return carprice;
+	}
+	public void setCarprice(int carprice) {
+		this.carprice = carprice;
+	}
+	
+
+}
