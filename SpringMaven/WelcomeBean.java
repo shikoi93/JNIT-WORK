cb05ef25dcecef38373b0953a48524a655a1438c
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-06-23 15:23:40 -0500

    Add files via upload

---
+++ SpringMaven/WelcomeBean.java
@@ -0,0 +1,15 @@
+package springmavenapp;
+
+public class WelcomeBean {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
